5191561e62109139bf6c2f08241908afb2ae8807
TuongDuong <tuongdm@sixthgearstudios.com>
2016-06-21 13:58:16 +0700

    add

---
+++ Assets/Shake/ChangeColor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangeColor : MonoBehaviour {
+
+  public Material mat;
+
+  void LateUpdate() {
+		Renderer renderer = GetComponent<Renderer> ();
+		renderer.material = mat;
+  }
+}
---
+++ Assets/Shake/PlayAnim.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnim : MonoBehaviour {
+
+	public float delayTime;
+	public float attackTime;
+	private tk2dSpriteAnimator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<tk2dSpriteAnimator> ();
+		if (anim != null) {
+			InvokeRepeating ("PlayAnimation", delayTime, attackTime);
+		}
+	}
+
+	void PlayAnimation() {
+		anim.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
